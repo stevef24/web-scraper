fe5b8681825e25a704d6b4830fc29340d4514f20
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-07-06 09:57:31 +0100

    added sort function and test cases

--- report.js
+++ report.js
@@ -1,3 +1,12 @@
 function sortPages(pages) {
 	// Sort pages by page number
+	const pagesArray = Object.entries(pages);
+
+	pagesArray.sort((a, b) => {
+		return b[1] - a[1];
+	});
+
+	return pagesArray;
 }
+
+module.exports = { sortPages };
--- report.test.js
+++ report.test.js
@@ -1,4 +1,37 @@
 const { sortPages } = require("./report");
 const { test, expect } = require("@jest/globals");
 
-describe("prepare report for pages ", () => {});
+describe("prepare report for pages ", () => {
+	test("sort 2 pages ", () => {
+		const input = {
+			"https://wagslane.com/path": 1,
+			"https://wagslane.com": 3,
+		};
+		const actual = sortPages(input);
+		const expected = [
+			["https://wagslane.com", 3],
+			["https://wagslane.com/path", 1],
+		];
+		expect(actual).toEqual(expected);
+	});
+	test("sort 5 pages ", () => {
+		const input = {
+			"https://wagslane.com/path": 1,
+			"https://wagslane.com/path/1": 3,
+			"https://wagslane.com/hello": 19,
+			"https://wagslane.com/path/there": 12,
+			"https://wagslane.com/path/about/id": 4,
+			"https://wagslane.com/about": 89,
+		};
+		const actual = sortPages(input);
+		const expected = [
+			["https://wagslane.com/path", 1],
+			["https://wagslane.com", 3],
+			["https://wagslane.com/path/about/id", 4],
+			["https://wagslane.com/path/there", 12],
+			["https://wagslane.com/hello", 19],
+			["https://wagslane.com/about", 89],
+		];
+		expect(actual).toEqual(expected);
+	});
+});
